ceddf5e403052386a777e9541b1f24baa2344f4c
Daniel E. Antonsen <danieleantonsen@gmail.com>
2024-07-23 16:34:09 +0200

    updated file

--- src/Nbody.py
+++ src/Nbody.py
@@ -9,14 +9,24 @@ import matplotlib.pyplot as plt
 
 class NBody:
 
-    def __init__(self, N: int, T: int, G: float = 1.0) -> None:
-        # Number of particles
+    def __init__(self, N: int, 
+                 max_t: int, 
+                 G: float = 1.0, 
+                 screen_size: tuple = (1.0, 1.0),
+                 grid_points: int = 1000
+                 ) -> None:
+        # Number of particles, graviatational constant and total 
         self.N = N
         self.G = G
+        self.max_t = max_t
+        
+        # Setting size of screen
+        self.screen_size = screen_size
+        self.x = np.linspace(-screen_size[0], screen_size[0], grid_points)
+        self.y = np.linspace(-screen_size[1], screen_size[1], grid_points)
 
         # List containg all particles
         self.particles = []
-
 
     
     def Compute_force(self, particle1: np.ndarray, particle2: np.ndarray) -> float:
