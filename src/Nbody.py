b824a8978f3baa31458272685f458af3c2fcd64d
Daniel E. Antonsen <danieleantonsen@gmail.com>
2025-12-28 16:57:06 +0100

    Updated files

--- src/Nbody.py
+++ src/Nbody.py
@@ -4,8 +4,9 @@ __author__ = 'Daniel Elisabethsønn Antonsen, Applied physics and mathematics'
 
 # Importing libraries and modules
 import numpy as np
-from itertools import combinations
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.colors as mcolors
 
 class nBody:
 
@@ -14,20 +15,20 @@ class nBody:
                  max_t: int = 365, 
                  G: float = 2.9591220828559093e-4,  # AU^3 / (solar_mass * day^2)
                  time_points: int = 1000,
-                 masses: None | list = None, 
-                 speed_factor: float = 0.1,
+                 masses: None | list = None,
                  pos: None | list[list[float]] = None,
-                 speed: None | list[list[float]] = None
+                 speed: None | list[list[float]] = None,
+                 colors: None | list[str] = None
                  ) -> None:
         # Number of particles, graviatational constant and total 
         self.N = N
         self.G = G 
         self.max_t = max_t
-        self.speed_factor = speed_factor
         self.time, self.dt = np.linspace(0, max_t, time_points, retstep = True)
-        
-        # Combinations for computing forces
-        self.pairs = list(combinations(range(N), 2))
+
+        # Colors for the different particles
+        diff_cols = [value for key, value in mcolors.TABLEAU_COLORS.items()]
+        self.colors = np.random.choice(diff_cols, size = N) if colors == None else colors
 
         # Position and velocity for the different particles
         self.pos = np.random.uniform(-1.0, 1.0, size = (N, 2)) if pos == None else np.array(pos)
@@ -56,8 +57,8 @@ class nBody:
         """
         Numerical scheme for updating position and velocity
         """
-        self.vel = self.vel + a * self.dt
-        self.pos = self.pos + self.vel * self.dt
+        self.vel += a * self.dt
+        self.pos += self.vel * self.dt
     
     def run(self) -> None:
         """
@@ -89,15 +90,29 @@ class nBody:
             # Storing position
             self.particles[i] = self.pos
 
-        plt.figure()
-        for t in range(len(self.time)):
-            plt.clf()
-            plt.grid()
-            plt.title(f'Day: {self.time[t]:.2f}')
-            for n in range(self.N):
-                plt.plot(*self.particles[t, n], 'o')
-            plt.xlim(self.particles[:, :, 0].min() - 1.0, self.particles[:, :, 0].max() + 1.0)
-            plt.ylim(self.particles[:, :, 1].min() - 1.0, self.particles[:, :, 1].max() + 1.0)
-            plt.pause(0.01)
+        self.animate()
+
+    def animate(self, fps: int = 60) -> None:
+        """
+        Animation of the N-body system
+        """
+        fig, ax = plt.subplots()
+        ax.set_xlim(self.particles[:, :, 0].min() - 1.0, self.particles[:, :, 0].max() + 1.0)
+        ax.set_ylim(self.particles[:, :, 1].min() - 1.0, self.particles[:, :, 1].max() + 1.0)
+        scat = ax.scatter([], [], c = self.colors)
+        plt.grid(True)
+        plt.xlabel(r'x (AU)')
+        plt.ylabel(r'y (AU)')
+
+        def update(frame):
+            scat.set_offsets(self.particles[frame])
+            ax.set_title(f'Day: {self.time[frame]:.0f}, FPS {fps}')
+            return scat,
+
+        ani = FuncAnimation(fig, update, frames = len(self.time), blit = True, interval = fps)
         plt.show()
-        
+
+
+
+
+
